4b9c45b3a7bf53a4921c412dcc8255525afbcc53
Rafael Constantino <118937917+rafaelconstantino13@users.noreply.github.com>
2022-12-06 14:51:40 -0300

    mudança na pasta

---
+++ script.js
@@ -0,0 +1,11 @@
+function laranja() {
+    document.body.style.backgroundColor = "#ff4500";
+}
+
+function amarelo() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function azul() {
+    document.body.style.backgroundColor = "blue";
+}
